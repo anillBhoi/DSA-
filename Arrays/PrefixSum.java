19691ed4e1ae53bd23e74fb15697e3d3c43fe365
Anilb08 <anilbhoi7740@gmail.com>
2025-02-22 22:22:48 +0530

    PrefixSum303

---
+++ Arrays/PrefixSum.java
@@ -0,0 +1,35 @@
+// leetcode 303 class NumArray {
+
+// using PrefixSum algorithm for efficency 
+// tc = O(n)+O(1) = O(n)
+
+class NumArray {
+    private int prefixSum[];
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        prefixSum = new int[n];
+
+
+        // find the prefix sum 
+        prefixSum[0] = nums[0];
+        for(int i=1; i<n; i++ ){
+            prefixSum[i] = prefixSum[i-1]+nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+          // corner case 
+          if(left == 0){
+            return prefixSum[right];
+          }
+
+          return prefixSum[right] - prefixSum[left-1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
