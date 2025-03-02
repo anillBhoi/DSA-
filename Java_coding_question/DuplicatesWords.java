bd483f4d6aaf23593668eaadb9a24efe8be88739
Anilb08 <anilbhoi7740@gmail.com>
2025-03-02 10:09:42 +0530

    Longest Substring without repeating 3

---
+++ Java_coding_question/DuplicatesWords.java
@@ -0,0 +1,13 @@
+// Q.1 Write a function to find out duplicate words in a 
+//given string?
+public class DuplicatesWords {
+    public static void duplicateWords(String string){
+        // step 1.convert in small letters 
+        // step 2. convert strings into words
+        // user two loops 
+    }
+
+    public static void main(String[] args) {
+        String string = "Big black bug bit a big black dog on his big black nose";
+    }
+}
