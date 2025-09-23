a47688a3a163077df463acd8eaec5b7784f2109d
Anilb08 <anilbhoi7740@gmail.com>
2025-09-23 18:58:24 +0530

    Evaluate Reverse Polish Notation 150

---
+++ Stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,41 @@
+package Stack;
+
+public class EvaluateReversePolishNotation {
+    
+
+    public int evalRPN(String[] tokens) {
+        String operators = "+-*/";
+        Stack<String>s = new Stack<>();
+
+        for(String tk: tokens){
+           if(!operators.contains(tk)){
+            s.push(tk);
+           } else{
+            int a = Integer.parseInt(s.pop());
+            int b = Integer.parseInt(s.pop());
+
+            int index = operators.indexOf(tk);
+
+             switch(index){
+                case 0:
+                 s.push(String.valueOf(a+b));
+                  break;
+
+                case 1:
+                 s.push(String.valueOf(b - a));
+                  break;
+
+                case 2:
+                s.push(String.valueOf(a * b));
+                 break;
+
+                case 3:
+                 s.push(String.valueOf(b / a));
+                    break;
+             }
+
+           }
+        }
+        return Integer.parseInt(s.pop());
+    }
+}
